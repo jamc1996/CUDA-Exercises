0df0509b4b69e50298026309c3fb0ffbed27c329
John Cormican <jcormica@cuda01.tchpc.tcd.ie>
2019-02-26 17:03:44 +0000

    changed timing function to include alloc timing.

--- matreduce.cu
+++ matreduce.cu
@@ -29,16 +29,44 @@ __global__ void GPU_col_reduce(float* Vgpu, float* aDatagpu, int n, int m)
 	
 }
 
-extern void run_tests(int n, int m, int seed)
+__global__ void GPU_sum_vec(float* Vgpu, int n, float *sum)
+{
+	int i = n/2;
+	int idx = blockIdx.x*blockDim.x + threadIdx.x;
+	if (idx < i)
+	{
+		sum[idx] = Vgpu[idx*2] + Vgpu[(idx*2)+1];
+	}
+	else if (idx = i && n%2 == 1)
+	{
+		sum[idx] = Vgpu[idx];
+	}
+	i+= n%2;
+	while (i!=2)
+	{
+		i /= 2;
+		if (idx < i)
+		{
+		  sum[idx]+= sum[i-1-idx];
+		}		
+		i += i%2;	
+	}
+	if (idx == 0)
+	{
+		sum[0] += sum[1];
+	}
+}
+
+extern void run_time_tests(int n, int m, int seed)
 {
 	int i;
 	clock_t begin, end;
 	double time_CPUr, time_CPUc, time_GPUr, time_GPUc;
-
+	double time_CPUredrow, time_CPUredcol, time_GPUredrow, time_GPUredcol;
 	float *aData, *Arowred, *Acolred;
 	float** A;
 	
-	float *adgpu, *argpu, *acgpu;
+	float *adgpu, *argpu, *acgpu, *rsum_gpu, *csum_gpu;
 	
 	srand(seed);
 	aData = (float*) malloc(sizeof(float)*n*m);
@@ -46,11 +74,7 @@ extern void run_tests(int n, int m, int seed)
 	Arowred = (float*) malloc(sizeof(float)*n);
 	Acolred = (float*) malloc(sizeof(float)*m);	
 
-
 	cudaMalloc ((void**) &adgpu, sizeof(float)*n*m);
-	cudaMalloc ((void**) &argpu, sizeof(float)*n);
-	cudaMalloc ((void**) &acgpu, sizeof(float)*m);
-
 	
 
 	for ( i=0; i<n*m; i++)
@@ -65,9 +89,7 @@ extern void run_tests(int n, int m, int seed)
 	memset(Arowred,0.0,sizeof(float)*n);
 	memset(Acolred,0.0,sizeof(float)*m);
 		
-	cudaMemcpy(adgpu,aData,sizeof(float)*n*m, cudaMemcpyHostToDevice);
-	cudaMemcpy(argpu,Arowred,sizeof(float)*n, cudaMemcpyHostToDevice);
-	cudaMemcpy(acgpu,Acolred,sizeof(float)*m, cudaMemcpyHostToDevice);
+cudaMemcpy(acgpu,Acolred,sizeof(float)*m, cudaMemcpyHostToDevice);
 
 	int block_size = 8;
 	dim3 dimBlock(block_size);
@@ -90,8 +112,13 @@ extern void run_tests(int n, int m, int seed)
 	time_CPUc = (double)(end - begin)/CLOCKS_PER_SEC;	
 
 	begin = clock();
+	
+	cudaMalloc ((void**) &argpu, sizeof(float)*n);
+	cudaMemcpy(adgpu,aData,sizeof(float)*n*m, cudaMemcpyHostToDevice);
 
 	GPU_row_reduce<<<dimGrid,dimBlock>>>(argpu,adgpu,n,m);
+
+	cudaMemcpy(argpu,Arowred,sizeof(float)*n, cudaMemcpyDeviceToHost);
 
 	end = clock();
 	
@@ -99,12 +126,26 @@ extern void run_tests(int n, int m, int seed)
 
 	begin = clock();
 
+	cudaMalloc ((void**) &acgpu, sizeof(float)*m);
 	GPU_col_reduce<<<dimGrid,dimBlock>>>(acgpu,adgpu,n,m);
 
 	end = clock();
 	
 	time_GPUc = (double)(end - begin)/CLOCKS_PER_SEC;
 
+	begin = clock();
+	
+	float total;
+
+	CPU_sum_vec(&total,Arowred,n);
+
+	end = clock();
+
+	time_CPUredrow = (double)(end - begin)/CLOCKS_PER_SEC;
+
+
+	//cudaMalloc ((void**) &sum_gpu, sizeof(float)*n);
+	
 	printf("Time of CPU on row reduction: %lf\n",time_CPUr);
 	printf("Time of CPU on column reduction: %lf\n",time_CPUc);
 	printf("Time of GPU on row reduction: %lf\n",time_GPUr);
