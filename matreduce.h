094fbd6427854f1f2f7605b67f2761cbc8cc5ee4
John Cormican <jcormica@cuda01.tchpc.tcd.ie>
2019-02-26 17:02:55 +0000

    changed function names

--- matreduce.h
+++ matreduce.h
@@ -1,11 +1,14 @@
 #ifndef MATREDUCE_H
 #define MATREDUCE_H
 
-__global__ void GPU_col_reduce(float* Vgpu, float* aDatagpu, int n, int m);
-__global__ void GPU_row_reduce(float* Vgpu, float* aDatagpu, int n, int m);
-void CPU_row_reduce(float* V, float** A, int n, int m);
-void CPU_col_reduce(float* V, float** A, int n, int m);
-void CPU_sum_vec(float* tot, float* V, int n);
-
+#ifdef __cplusplus
+extern "C" 
+{
+	void run_time_tests(int n, int m, int seed);
+	void CPU_row_reduce(float* V, float** A, int n, int m);
+	void CPU_col_reduce(float* V, float** A, int n, int m);
+	void CPU_sum_vec(float* tot, float* V, int n);
+}
+#endif
 
 #endif
