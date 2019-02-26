2059ba4b1ffa64c2873d4506389c7bfb26d2778c
John Cormican <jcormica@cuda01.tchpc.tcd.ie>
2019-02-26 17:02:28 +0000

    getopt used for timing options.

--- main.c
+++ main.c
@@ -1,10 +1,42 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "matreduce.h"
 
-extern void run_tests(int n, int m, int seed);
+extern void run_time_tests(int n, int m, int seed);
 
-int main()
+int main(int argc, char* argv[])
 {
-  run_tests(10000,10000,123456);     
+  int n = 10000;
+  int m = 10000;
+  int seed = 123456;
+  int timeflag = 0;
+  int c=0;
+  while ((c=getopt(argc,argv,"tn:m:s:")) != -1)
+  {
+    switch(c)
+    {
+    case 'n':
+      n = atoi(optarg);
+      break;
+    case 'm':
+      m = atoi(optarg);
+      break;
+    case 's':
+      seed = atoi(optarg);
+      break;
+    case 't':
+      timeflag = 1;
+      break;
+    case '?':
+      return 1;
+    }
+  }
+ if (timeflag == 1)
+  {
+   run_time_tests(n,m,seed);     
+  } 
 
   return 0;
 }
